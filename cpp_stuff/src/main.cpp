25b2344dc5debf91499fdc9f880243c91c61a057
Arttu Valo <aevalo@fedora-mate.vmdomain>
2013-04-22 20:04:54 +0300

    aev: Added example to dynlib.
    Added simple example for dynlib code.

--- cpp_stuff/src/main.cpp
+++ cpp_stuff/src/main.cpp
@@ -3,14 +3,45 @@
 
 typedef const char* (*version_func_t)();
 
+class compressor
+{
+  public:
+    virtual ~compressor() {_dl.close();}
+
+    virtual bool get_syms() = 0;
+    virtual const char* version() = 0;
+
+  protected:
+
+    compressor(const char* path) {_dl.open(path);}
+    compressor(const compressor& c);
+    compressor& operator=(const compressor& rhs);
+
+
+    dynlib _dl;
+};
+
+class zlib_compressor : public compressor
+{
+  public:
+    zlib_compressor() : compressor("libz.so"), version_func(NULL) {}
+    virtual ~zlib_compressor() {}
+
+    virtual bool get_syms() {version_func = reinterpret_cast<version_func_t>(_dl.sym("zlibVersion"));}
+    virtual const char* version() {if(version_func){return (version_func)();}return "";}
+
+  protected:
+
+
+  private:
+
+    version_func_t version_func;
+};
+
 int main(int argc, char* argv[])
 {
-  dynlib zlib("libz.so");
-  version_func_t zlibVersion = reinterpret_cast<version_func_t>(zlib::sym("zlibVersion", h));
-  if (zlibVersion)
-  {
-    fprintf(stdout, "zlib version: %s\n", (zlibVersion)());
-  }
-  zlib::close();
+  zlib_compressor zlib;
+  zlib.get_syms();
+  fprintf(stdout, "zlib version: %s\n", zlib.version());
   return 0;
 }
